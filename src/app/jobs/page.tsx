197538b442a9f74202f231a032e1384e37a4d109
Farhad gul <164610570+Farhadali987@users.noreply.github.com>
2025-01-29 04:36:09 -0800

    Update page.tsx

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from '../components/header'
+import Footer from '../components/footer'
 const Jobs = () => {
   return (
     <>
     <Header />
     <div><h1 className='pt-[270px] font-bold text-[24px] text-center'>Jobs/ Internships coming soon.</h1></div>
-
+< Footer />
 
     </>
   )
